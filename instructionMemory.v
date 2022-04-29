9c64292153280414f75693fa8aa9ddfb27e8f684
shubhamksavita <shubhamsavita89@gmail.com>
2022-04-29 10:31:44 -0700

    32bit-MIPS: Adding Instruction Memory and Sample Instruction                 (May need changes later)

---
+++ instructionMemory.v
@@ -0,0 +1,25 @@
+//instruction Memory 
+// Fig. 4.5 page 252, COD 5th edition
+
+module instructionMemory(
+	output reg [31:0] inst_out,
+	input [31:0] inst_in);
+
+reg [31:0] inst_mem[0:1023];
+
+
+initial 
+	begin
+	$readmemb("sample_instructions.txt",inst_mem);
+	end
+
+always @(inst_in) begin
+
+	inst_out <= inst_mem[inst_in];
+
+end
+
+//assign inst_out = inst_mem[inst_in]; 
+
+endmodule
+	
