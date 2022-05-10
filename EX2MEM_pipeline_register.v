0f6d487bb8eb685018532c8557cda2bb4d005575
helljumperjoe <101843300+helljumperjoe@users.noreply.github.com>
2022-05-09 22:41:26 -0700

    Update EX2MEM_pipeline_register.v

--- EX2MEM_pipeline_register.v
+++ EX2MEM_pipeline_register.v
@@ -1,14 +1,14 @@
 
 
-module EXE2MEM (clk, rst, WB_EN_IN, MEM_R_EN_IN, MEM_W_EN_IN, ALUResIn, STValIn, destIn,Zeroflag
-                          WB_EN,    MEM_R_EN,    MEM_W_EN, Zero,  ALURes,   STVal,   dest);
-  input clk, rst, Zeroflag;
+module EXE2MEM (clk, rst, WB_EN_IN, MEM_R_EN_IN, MEM_W_EN_IN, ALUResIn, STValIn, destIn,Zeroflag, branch
+                          WB_EN,    MEM_R_EN,    MEM_W_EN, Zero,branch_out,  ALURes,   STVal,   dest);
+  input clk, rst, Zeroflag,branch;
 
   input WB_EN_IN, MEM_R_EN_IN, MEM_W_EN_IN;
   input [4:0] destIn;
   input [31:0]  ALUResIn, STValIn;
 
-  output reg WB_EN, MEM_R_EN, MEM_W_EN, Zero;
+  output reg WB_EN, MEM_R_EN, MEM_W_EN, Zero,branch_out;
   output reg [4:0] dest;
   output reg [31:0] ALURes, STVal;
 
@@ -24,6 +24,7 @@ module EXE2MEM (clk, rst, WB_EN_IN, MEM_R_EN_IN, MEM_W_EN_IN, ALUResIn, STValIn,
       ALURes <= ALUResIn;
       STVal <= STValIn;
       dest <= destIn;
+      branch_out<=branch;
     end
   end
 endmodule // EXE2MEM
